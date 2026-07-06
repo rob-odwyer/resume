9872ce863fc6163529efd8218c6626d6fe48705c
Rob ODwyer <odwyerrob@gmail.com>
2026-07-06 11:11:47 -0700

    initial update for 2026

--- build.sh
+++ build.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-set -e
-
-# Remove sensitive fields from HTML version
-jq 'del(.basics.phone)' resume.base.json > resume.json
-resume export --theme short resume.html
-resume export --theme elegant index.html
-
-# Leave all fields for PDF
-jq '.' resume.base.json > resume.json
-resume export --theme short --format pdf resume.pdf
+npx resumed export resume.json -o resume.pdf
--- upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-BUCKET="s3://rob.odwyer.cc/"
-
-aws s3 sync --dryrun --exclude ".git*" --exclude "*.sh" --acl "public-read" ./ ${BUCKET}
-
-read -p "Press any key to proceed" CONT
-echo
-
-aws s3 sync --exclude ".git*" --exclude "*.sh" --acl "public-read" ./ ${BUCKET}
